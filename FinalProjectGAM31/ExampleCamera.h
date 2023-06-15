66afd967099c70b28974bc05c0a3318a9f3a367e
RPichen <52576856+RPichen@users.noreply.github.com>
2023-06-14 21:00:05 -0500

    Add files via upload

---
+++ FinalProjectGAM31/ExampleCamera.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Camera/CameraActor.h"
+#include "ExampleCamera.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FINALPROJECTGAM31_API AExampleCamera : public ACameraActor
+{
+	GENERATED_BODY()
+	
+};
